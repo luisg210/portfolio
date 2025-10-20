1666f2daa96fbc44320226ce4fcf220f4f4e1ee4
luisg210 <luisger23000@gmail.com>
2025-10-20 11:47:49 -0600

    Corrigiendo base url 8

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,21 @@
 set -e
 
 # build
+echo "Building project..."
 npm run build
 
 # navigate into the build output directory
+echo "Entering dist..."
 cd dist
 
 # place .nojekyll to bypass Jekyll processing
+echo "Creating .nojekyll..."
 echo > .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+echo "Initializing git..."
 git init
 git checkout -b gh-pages
 git add -A
@@ -24,6 +28,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:luisg210/portfolio.git gh-pages
+echo "Pushing to GitHub..."
+git push -f https://github.com:luisg210/portfolio.git gh-pages
 
+echo "cd -"
 cd -
